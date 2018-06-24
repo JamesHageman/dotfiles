faae5803dd3f323e152656a9d7e63843f94540d3
James Hageman <jamesd@hageman.ca>
2018-06-23 18:30:11 -0700

    brew leaves

--- copy.sh
+++
@@ -1,5 +0,0 @@
-cp ~/.vimrc .vimrc
-cp ~/.zshrc .zshrc
-cp ~/.tmux.conf .tmux.conf
-cp ~/.gitignore .gitignore_global
-
--- link.sh
+++ link.sh
@@ -1,5 +1,10 @@
 set -ex
+
 ln .zshrc ~/.zshrc
 ln .vimrc ~/.vimrc
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 ln .editorconfig ~/.editorconfig
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+cat brew_leaves.txt | xargs brew install
